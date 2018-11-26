b15c0030f3c3e221c37b9c68a1d8225269ea93b2
ekorslin <eric.korslin@gmail.com>
2018-11-26 15:53:52 -0600

    corrected server.js file

--- server.js
+++ server.js
@@ -81,11 +81,11 @@ app.get("/scrape", function (req, res) {
             // { $addFields: { artId: { "$toString": "articleId" }}},
             // { $addFields: { artId: { "$toString": "$_id" }}},
             // { "$addFields": { "artId": { "$toObjectId": "$articleId" }}},
-            { $addFields: { convertedId: { $toObjectId: "$articleId" }}},
+            // { $addFields: { convertedId: { $toObjectId: "$articleId" }}},
             { $lookup: {
               from: "comments",
               localField: "_id",
-              foreignField: "convertedId",
+              foreignField: "articleId",
               as: "comments",
             }}]
             , function(err, response) {
